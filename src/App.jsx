ed9fcf9c051a9c3717a454310b838d5926be5b78
luis-luciqm <redbullfc18@gmail.com>
2021-07-22 21:56:34 -0300

    Instalando Material UI para auxilio no front-end

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,11 @@ import './styles/global.scss';
 import { Teste } from './components/Teste'
 
 export function App() {
-    return <Teste />
+    return (
+        <div>
+            <h1>This is a new project</h1>
+            <Teste />
+            <h2>End a project</h2>
+        </div>
+    )
 }
